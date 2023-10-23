ddf7afc6815e139df261789fd34cd8dd91a90b07
nuriddin5 <nuriddinqurbonboyev@mail.ru>
2023-10-23 16:41:27 +0500

    first commit

---
+++ Payme/Payme10A.java
@@ -0,0 +1,7 @@
+package ATM;
+/**
+ * User:t.me/supermatematikuz
+ * Date:23.10.2023 11:10
+ */
+public class Payme10A {
+}
---
+++ Payme/PaymeConstants.java
@@ -0,0 +1,7 @@
+package Payme;
+/**
+ * User:t.me/supermatematikuz
+ * Date:23.10.2023 11:19
+ */
+public class PaymeConstants {
+}
